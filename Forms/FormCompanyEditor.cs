a04ff6a8e09b1f8f399a081e7f9876f985d37ef1
IsabelaAV <isabela.arango.verona@gmail.com>
2025-09-28 17:11:22 -0500

    Optimización de usings y manejo de excepciones
    Se eliminaron `using` innecesarios en los archivos **EmpresaRepository.cs**, **FormCompanyEditor.cs** y **FormCompanyList.cs**, reduciendo el desorden y optimizando las dependencias.
    En **EmpresaService.cs**, se añadieron bloques `try-catch` a todos los métodos del servicio para mejorar el manejo de excepciones, proporcionando mayor robustez y mensajes de error más descriptivos.

--- Forms/FormCompanyEditor.cs
+++ Forms/FormCompanyEditor.cs
@@ -1,15 +1,5 @@
-﻿using PruebaTecnica.Data;
-using PruebaTecnica.Models;
+﻿using PruebaTecnica.Models;
 using PruebaTecnica.Services.Impl;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
 
 namespace PruebaTecnica.Forms
 {
