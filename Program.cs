edba787214bd7c2427f23d7096c7ff6e564e78f4
IsabelaAV <isabela.arango.verona@gmail.com>
2025-09-28 10:08:07 -0500

    Se crea appsettings.json para la cadena de conexión, y configuración en AppDbContext.cs y Program.cs para el correcto funcionamiento de EF

--- Program.cs
+++ Program.cs
@@ -1,3 +1,9 @@
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using PruebaTecnica.Data;
+using Microsoft.EntityFrameworkCore;
+
+
 namespace PruebaTecnica
 {
     internal static class Program
@@ -8,6 +14,29 @@ namespace PruebaTecnica
         [STAThread]
         static void Main()
         {
+            // Configuración
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+
+            var configuration = builder.Build();
+
+            // Inyección de dependencias
+            var services = new ServiceCollection();
+            services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddTransient<Form1>();
+
+            var provider = services.BuildServiceProvider();
+
+            // Ejecutar migraciones al arrancar
+            using (var scope = provider.CreateScope())
+            {
+                var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+                context.Database.Migrate();
+            }
+
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
